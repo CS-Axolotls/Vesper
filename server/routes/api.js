73eaab47bbe9f23a022750d62448efa4e23cd0df
Eric Park <ericparkmusic@gmail.com>
2021-05-22 12:24:06 -0700

    Set up request for movie list

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const express = require('express');
 
-const fileController = require('../controllers/fileController');// TODO double check this
+const movieController = require('../controllers/movieController.js'); // TODO double check this
 
 const router = express.Router();
 
 // ADD STARTER DATA REQUEST ROUTE HANDLER HERE
-router.get('/', fileController.getCharacters, (req, res) => { // TODO change this
-    res.status(200).json({ characters: res.locals.characters });// TODO change this
+router.get('/', movieController.getPopularMovies, (req, res) => {
+  // changed this
+  res.status(200).send(res.locals.movieResults);
 });
 
 module.exports = router;
