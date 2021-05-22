4ad790a71577e051e59c4b06f1a32d95ae3eaf13
Tyler Martinez <tm6002005@gmail.com>
2021-05-22 10:56:24 -0700

    'updating app component

--- client/App.jsx
+++ client/App.jsx
@@ -3,11 +3,7 @@ import Usher from './components/Usher.jsx';
 
 class App extends React.Component {
   render() {
-<<<<<<< HEAD
-    return <p>yoyo</p>;
-=======
     return <Usher></Usher>;
->>>>>>> 3f95c6a643328219782c510625336bab8ec10615
   }
 }
 
