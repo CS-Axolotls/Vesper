3f95c6a643328219782c510625336bab8ec10615
Eric Park <ericparkmusic@gmail.com>
2021-05-21 20:35:37 -0700

    Add Bartender.jsx and Usher.jsx to components folder

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import Usher from './components/Usher.jsx';
 
 class App extends React.Component {
   render() {
-    return <p>YO</p>;
+    return <Usher></Usher>;
   }
 }
 
