034dccf68afdb10a2fba6bc73194dc36399e51a2
Eric Park <ericparkmusic@gmail.com>
2021-05-21 19:12:20 -0700

    Set up file structure and webpack

---
+++ client/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <p>YO</p>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./client/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+  devServer: {
+    publicPath: '/',
+    proxy: {
+      '/api': 'http://localhost:3000/',
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
