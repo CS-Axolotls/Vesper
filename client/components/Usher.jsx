d11fc516679a24e3e62be29c2473384ecda30c9a
Eric Park <ericparkmusic@gmail.com>
2021-05-22 15:04:58 -0700

    Successfully fetching movies from server

--- client/components/Usher.jsx
+++ client/components/Usher.jsx
@@ -1,11 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Bartender from './Bartender.jsx';
+import MovieCard from './MovieCard.jsx';
 
 const Usher = () => {
   const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+    fetch('/api')
+      .then((data) => data.json())
+      .then((updatedData) => {
+        if (isMounted) {
+          setMovies(updatedData);
+        }
+      })
+      .catch(() => console.log('fetching from movies in Usher'));
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  if (movies.length === 0) {
+    return <p>Loading...</p>;
+  }
+
+  const movieCards = [];
+
+  for (let i = 0; i < movies.length; i++) {
+    movieCards.push(<MovieCard movie={movies[i]}></MovieCard>);
+  }
   return (
     <div>
-      <div>Usher comp</div>
+      {movieCards}
       <Bartender></Bartender>
     </div>
   );
