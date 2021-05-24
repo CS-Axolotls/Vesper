c85a232496275e292a2fc67be4acacdc968b44e3
Eric Park <ericparkmusic@gmail.com>
2021-05-23 18:30:40 -0700

    set up cocktail api call with genre input

--- server/controllers/cocktailController.js
+++ server/controllers/cocktailController.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch');
 
+const genreDrinks = require('../data/genreDrinks.json');
 const cocktailController = {};
 
 cocktailController.getRandomDrink = (req, res, next) => {
@@ -18,12 +19,15 @@ cocktailController.getRandomDrink = (req, res, next) => {
 cocktailController.getSpecificDrink = (req, res, next) => {
   // Double check req.query here: http://expressjs.com/en/api.html
   // We also have to pay attention to the body parser
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.query.genre}`; // The request should look something like this: localhost:3000/api/specificDrink?drink=margarita
-  console.log(url);
+
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${
+    genreDrinks[req.query.genre]
+  }`; // The request should look something like this: localhost:3000/api/specificDrink?genre=number
+  //   console.log('supposed to be correct drink' + genreDrinks[req.query.genre]);
   fetch(url)
     .then((data) => data.json())
     .then((updatedData) => {
-      res.locals.drinkResults = updatedData.drinks;
+      res.locals.drinkResults = updatedData.drinks[0];
       return next();
     })
     .catch((err) => {
